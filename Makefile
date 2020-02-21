3bc60eba86812906cc49783e0c44c107882ee34d
viniciussousazup <vinicius.sousa@zup.com.br>
2020-02-20 23:19:00 -0300

    hello world

--- Makefile
+++ Makefile
@@ -1,17 +1,6 @@
 #Makefiles
-TERRAFORM=aws/terraform
-DARWIN=darwin/deploy
-WEBHOOK=github/zup-webhook
-NAVIGATE_HANDBOOK=github/navigate-handbook
-SEARCH_HANDBOOK=github/search-handbook
-JENKINS_JOB=jenkins/jobs
-SC_COFFEE=scaffold/coffee
-SC_SPRING=scaffold/spring-iti
-KAFKA=kafka
-VIVO=vivo/deploy
-DOCKER=docker/compose
-KUBERNETES=kubernetes/core
-FORMULAS=$(TERRAFORM) $(DARWIN) $(WEBHOOK) $(JENKINS_JOB) $(SC_COFFEE) $(SC_SPRING) $(KAFKA) $(VIVO) $(DOCKER) $(NAVIGATE_HANDBOOK) $(SEARCH_HANDBOOK) $(KUBERNETES)
+HELLO_WORLD=github/hello-world
+FORMULAS=$(HELLO_WORLD)
 
 PWD_INITIAL=$(shell pwd)
 
---
+++ github/hello-world/src/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	destinationBranch := os.Getenv("BRANCH")
+	push := os.Getenv("PUSH")
+	currentBranch := execCommand("git rev-parse --abbrev-ref HEAD")
+
+	execCommand("git fetch")
+	execCommand("git branch -D " + destinationBranch)
+	execCommand("git checkout " + destinationBranch)
+	execCommand("git merge origin " + currentBranch)
+
+	if push == "true" {
+		execCommand("git push")
+		execCommand("git checkout " + currentBranch)
+	}
+}
+
+func execCommand(value string) string {
+	command := strings.Split(value, " ")[0]
+	params := strings.Split(value, " ")[1:]
+	log.Printf("Executing command: %v params: %v\n", command, params)
+	cmd := exec.Command(command, params...)
+	stdout, _ := cmd.StdoutPipe()
+	var outError bytes.Buffer
+	cmd.Stderr = &outError
+	cmd.Start()
+	scanner := bufio.NewScanner(stdout)
+	scanner.Split(bufio.ScanLines)
+	m := ""
+	for scanner.Scan() {
+		m += scanner.Text()
+		fmt.Println(m)
+	}
+	err := cmd.Wait()
+	if err != nil {
+		log.Fatalf("Failed to execute command %v\nParams: %v\nError: %v", command, params, outError.String())
+	}
+	return m
+}
