cf0b2b8eb1e0b9663a0c678296752e872e85f3c2
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2020-08-26 11:59:49 -0300

    Update shell template

--- templates/create_formula/languages/shell/src/formula/formula.sh
+++ templates/create_formula/languages/shell/src/formula/formula.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 runFormula() {
+  #In sh for receive inputs of CLI use: $SAMPLE_TEXT, $SAMPLE_LIST and $SAMPLE_BOOL for this example
   echo "Hello World! "
   echo "You receive $SAMPLE_TEXT in text. "
   echo "You receive $SAMPLE_LIST in list. "
